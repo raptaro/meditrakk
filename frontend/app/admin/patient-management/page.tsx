d9adaa2c843d6355a05691fa511cb1a93a45b94d
Alexander Acojido <alexacojido@gmail.com>
2025-10-21 22:04:04 +0800

    feat(admin-patient-management): connect to backend

--- frontend/app/admin/patient-management/page.tsx
+++ frontend/app/admin/patient-management/page.tsx
@@ -1,14 +1,27 @@
 "use client";
+import { useFetch } from "@/hooks/use-fetch";
+import { CrudTable } from "@/components/ui/crud-table";
+import { SkeletonDataTable } from "@/components/atoms/custom-skeleton";
 import { columns } from "./patient-management-columns";
 
-import { SkeletonDataTable } from "@/components/atoms/custom-skeleton";
-import { CrudTable } from "@/components/ui/crud-table";
-import { users } from "@/lib/placeholder-data";
+type User = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  role: "admin" | "doctor" | "secretary";
+  is_active: boolean;
+};
 
 export default function DemoPage() {
-  if (users === undefined) {
-    return <SkeletonDataTable />;
-  }
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useFetch<User[]>(`${process.env.NEXT_PUBLIC_API_BASE}/auth/users/`);
+
+  if (isLoading) return <SkeletonDataTable />;
+  if (error) return <div className="p-4 text-red-500">{error}</div>;
 
   return <CrudTable title="Users" columns={columns} data={users ?? []} />;
 }
