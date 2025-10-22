1dcce420840fbb94a3061d30053e00983d0f4eeb
Alexander Acojido <alexacojido@gmail.com>
2025-10-22 19:40:19 +0800

    feat(admin-doctor-management): added archived button

--- frontend/app/admin/doctor-management/page.tsx
+++ frontend/app/admin/doctor-management/page.tsx
@@ -3,6 +3,7 @@ import { useFetch } from "@/hooks/use-fetch";
 import { DataTable } from "@/components/ui/data-table";
 import { SkeletonDataTable } from "@/components/atoms/custom-skeleton";
 import { columns } from "./doctor-management-columns";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type User = {
   id: string;
@@ -22,8 +23,40 @@ export default function Page() {
     `${process.env.NEXT_PUBLIC_API_BASE}/user/users/?role=doctor&ordering=-date_joined`
   );
 
-  if (isLoading) return <SkeletonDataTable />;
-  if (error) return <div className="p-4 text-red-500">{error}</div>;
+  const {
+    data: archivedDoctors,
+    isLoading: isArchivedLoading,
+    error: archivedError,
+  } = useFetch<User[]>(
+    `${process.env.NEXT_PUBLIC_API_BASE}/user/users/archived/?role=doctor&ordering=-date_joined`
+  );
 
-  return <DataTable title="Doctors" columns={columns} data={doctors ?? []} />;
+  if (isLoading || isArchivedLoading) return <SkeletonDataTable />;
+  if (error || archivedError)
+    return <div className="p-4 text-red-500">{error || archivedError}</div>;
+
+  return (
+    <Tabs defaultValue="doctors">
+      <TabsList className="w-full rounded-none">
+        <TabsTrigger className="w-1/2 rounded-none" value="doctors">
+          Doctors
+        </TabsTrigger>
+        <TabsTrigger className="w-1/2 rounded-none" value="archived-doctors">
+          Archived Doctors
+        </TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="doctors">
+        <DataTable title="Doctors" columns={columns} data={doctors ?? []} />
+      </TabsContent>
+
+      <TabsContent value="archived-doctors">
+        <DataTable
+          title="Archived Doctors"
+          columns={columns}
+          data={archivedDoctors ?? []}
+        />
+      </TabsContent>
+    </Tabs>
+  );
 }
