61102d4bba6f48c044226c34fb7f29ad64e02721
Alexander Acojido <alexacojido@gmail.com>
2025-11-13 14:57:48 +0800

    fix(sidebar): trigger not appearing on mobile
    this is for the other sidebars

--- frontend/app/secretary/layout.tsx
+++ frontend/app/secretary/layout.tsx
@@ -17,8 +17,8 @@ export default function Page({
         <AppSidebar />
         <SidebarInset className="flex-1">
           <main className="flex-1">
-            <SidebarTrigger className="fixed md:hidden" />
             <Header />
+            <SidebarTrigger className="fixed top-0 md:hidden" />
             {children}
           </main>
         </SidebarInset>
