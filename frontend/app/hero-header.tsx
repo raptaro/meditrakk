e1a4c088f7a5b29d4d413dab5660d452047d4242
Alexander Acojido <alexacojido@gmail.com>
2025-11-14 14:31:08 +0800

    fix(hero-header): not responsive

--- frontend/app/hero-header.tsx
+++ frontend/app/hero-header.tsx
@@ -42,23 +42,6 @@ const HeroHeader = () => {
             <Link href="/login">Login</Link>
           </Button>
         </div>
-
-        <button className="md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        </button>
       </div>
     </nav>
   );
