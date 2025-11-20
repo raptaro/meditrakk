39b854ba3b70ebdc10b5a28b728f28e94c335fa8
Alexander Acojido <alexacojido@gmail.com>
2025-11-20 14:00:15 +0800

    fix(prisma): url is missing in datasource block db
    updated to prisma 7.0.0 and typescript 5.9.3

--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -1,9 +1,7 @@
-// lib/prisma.ts
 import { PrismaClient } from '@/prisma/generated/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+export const prisma = new PrismaClient({ adapter });
